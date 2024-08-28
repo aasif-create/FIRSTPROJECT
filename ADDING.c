e2cd01978e76c77f1d041fd045d8a74fe3f46ff7
aasif-create <147026508+aasif-create@users.noreply.github.com>
2024-08-28 20:40:35 +0530

    Add files via upload

---
+++ ADDING.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int a=10;
+  int b=20;
+  int sum;
+  sum=a+b;
+  printf("%d \n",sum);
+  printf("<-------------->\n");
+  int c,d,e;
+  printf("ENTER 1ST NUMBER :");
+  scanf("%d",&c);
+  printf("ENTER 2ND NUMBER :");
+  scanf("%d",&d);
+  e=c+d;
+  printf("THE SUM IS :%d",e);
+  return 0;
+}
